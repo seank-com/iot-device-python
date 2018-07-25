91fb08ba96574d8c70afea8121b19f889459e4cb
Sean Kelly <home@seank.com>
2018-07-24 17:59:26 -0700

    first version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # iot-device-python
 The minimal code for an IoT device to connect to an Azure IoTHub in python
+
+## Setup
+
+For Python 2.7 run the following:
+
+```bash
+$ pip install azure-iothub-device-client
+```
+
+For Python3 run the following:
+
+```bash
+$ pip3 install azure-iothub-device-client
+```
+
+If you see an error like the following 
+
+```
+
+```
+
+run
+
+```bash
+$ sudo apt-get install libboost-all-dev curl libcurl3
+```
+
+**Notice:** if you would like to install python3 run the following:
+
+```bash
+$ sudo apt-get install python3=3.5.1*
+```
--- device.py
+++ device.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for
-# full license information.
-
 import time
 import sys
 import iothub_client
@@ -17,42 +13,22 @@ CONNECTION_STRING = "[Device Connection String]"
 MSG_TXT = "{\"msg\": \"%s\"}"
 
 RECEIVE_CONTEXT = 0
+SEND_CONTEXT = 0
 
-SLEEP_TIME = 5000
+SLEEP_TIME = 15
 
-def receive_message_callback(message, counter):
+def receive_message_callback(message, user_context):
     message_buffer = message.get_bytearray()
     size = len(message_buffer)
-    print ( "Received Message [%d]:" % counter )
-    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode('utf-8'), size) )
-    map_properties = message.properties()
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
+    print ( "Received Message:  data = \"%s\" size=%d" % (message_buffer[:size].decode('utf-8'), size) )
     return IoTHubMessageDispositionResult.ACCEPTED
 
 def send_confirmation_callback(message, result, user_context):
-    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
-    map_properties = message.properties()
-    print ( "    message_id: %s" % message.message_id )
-    print ( "    correlation_id: %s" % message.correlation_id )
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-
-def print_last_message_time(client):
-    try:
-        last_message = client.get_last_message_receive_time()
-        print ( "Last Message: %s" % time.asctime(time.localtime(last_message)) )
-        print ( "Actual time : %s" % time.asctime() )
-    except IoTHubClientError as iothub_client_error:
-        if iothub_client_error.args[0].result == IoTHubClientResult.INDEFINITE_TIME:
-            print ( "No message received" )
-        else:
-            print ( iothub_client_error )
+    print ( "Confirmation received for message with result = %s" % result )
 
 def iothub_client_sample_run():
 
     try:
-
         # prepare iothub client
         client = IoTHubClient(CONNECTION_STRING, IoTHubTransportProvider.MQTT)
         # to enable MQTT logging set to 1
@@ -61,12 +37,12 @@ def iothub_client_sample_run():
 
         while True:
             # send a few messages every minute
-            print ( "IoTHubClient sending %d messages" % MESSAGE_COUNT )
+            print ( "IoTHubClient sending message" )
 
             msg_txt_formatted = MSG_TXT % "This is a test"
             message = IoTHubMessage(msg_txt_formatted)
 
-            client.send_event_async(message, send_confirmation_callback, message_counter)
+            client.send_event_async(message, send_confirmation_callback, SEND_CONTEXT)
             print ( "IoTHubClient.send_event_async accepted message for transmission to IoT Hub." )
 
             time.sleep(SLEEP_TIME)
@@ -76,8 +52,6 @@ def iothub_client_sample_run():
         return
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
-
-    print_last_message_time(client)
 
 if __name__ == '__main__':
     print ( "\nPython %s" % sys.version )
